4736111bf72be8a0dd00819be3c58d10ac2fb879
Patrick Stotko <stotko@cs.uni-bonn.de>
2024-12-06 15:41:15 +0100

    Fix stdgpu lib dir
    Upgrade stdgpu to latest version (#7083)
    
    Update CUDA requirements to > 11.5

--- 3rdparty/stdgpu/stdgpu.cmake
+++ 3rdparty/stdgpu/stdgpu.cmake
@@ -7,8 +7,8 @@ include(ExternalProject)
 ExternalProject_Add(
     ext_stdgpu
     PREFIX stdgpu
-    URL https://github.com/stotko/stdgpu/archive/e10f6f3ccc9902d693af4380c3bcd188ec34a2e6.tar.gz
-    URL_HASH SHA256=7bb2733b099f7cedc86d2aee7830d128ac1222cfafa34cbaa4e818483c0a93f6
+    URL https://github.com/stotko/stdgpu/archive/2588168d226bd17229dbf58d821549580791089d.tar.gz
+    URL_HASH SHA256=86e50789bbe21c57f64358c6acbd4481d56c1e45ce9ba1fb5c5c8482c3973215
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/stdgpu"
     UPDATE_COMMAND ""
     CMAKE_ARGS
@@ -28,5 +28,6 @@ ExternalProject_Add(
 
 ExternalProject_Get_Property(ext_stdgpu INSTALL_DIR)
 set(STDGPU_INCLUDE_DIRS ${INSTALL_DIR}/include/) # "/" is critical.
-set(STDGPU_LIB_DIR ${INSTALL_DIR}/lib)
+# set(STDGPU_LIB_DIR ${INSTALL_DIR}/lib)
+set(STDGPU_LIB_DIR ${INSTALL_DIR}/${Open3D_INSTALL_LIB_DIR})
 set(STDGPU_LIBRARIES stdgpu)
