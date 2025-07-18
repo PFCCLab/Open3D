f175a8d9d94b5d953c1f42382476bde12e2f4e56
HydrogenSulfate <490868991@qq.com>
2025-07-18 16:50:22 +0800

    Update __init__.py
    Signed-off-by: HydrogenSulfate <490868991@qq.com>

--- python/open3d/__init__.py
+++ python/open3d/__init__.py
@@ -112,7 +112,7 @@ def _insert_pybind_names(skip_names=()):
 import open3d.visualization
 _insert_pybind_names(skip_names=("ml",))
 
-__version__ = "0.18.0+7f9ae91e"
+__version__ = "@PROJECT_VERSION"
 
 if int(sys.version_info[0]) < 3:
     raise Exception("Open3D only supports Python 3.")
